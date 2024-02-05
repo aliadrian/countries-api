5af814d92e245691b8b07fd804ce44af2d80ce83
Adrian <adriannasrat@gmail.com>
2024-02-05 01:23:42 +0100

    added accessible name to select element

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,9 +5,9 @@ export const Dropdown = ({ selectedRegion, handleRegionChange, countries }) => {
 
   return (
     <div className='relative'>
-      <label>
-        <select htmlFor="Filtering the countries by region" className='md:w-full w-8/12 lg:px-12 shadow-lg py-6 md:px-6 px-4 border-r-8 border-white dark:border-darkElement dark:bg-darkElement dark:text-white' value={selectedRegion} onChange={(e) => handleRegionChange(e.target.value)}>
-          <option value="" className=''>Filter by Region</option>
+      <label htmlFor="Filtering the countries by region">
+        <select className='md:w-full w-8/12 lg:px-12 shadow-lg py-6 md:px-6 px-4 border-r-8 border-white dark:border-darkElement dark:bg-darkElement dark:text-white' value={selectedRegion} onChange={(e) => handleRegionChange(e.target.value)}>
+          <option value="">Filter by Region</option>
           {uniqueRegions.map(region => (
             <option className='dark:bg-darkBg dark:text-white' key={region} value={region}>{region}</option>
           ))}
