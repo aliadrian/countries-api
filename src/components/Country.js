9d7f44ab4f4d25871aed2a6cbbe28342c025c12e
Adrian <adriannasrat@gmail.com>
2024-02-05 01:02:20 +0100

    fixed alt text of evey flag

--- src/components/Country.js
+++ src/components/Country.js
@@ -6,7 +6,7 @@ const Country = ({ country }) => {
   let totalPopulation = population.toLocaleString("en");
   const flags = country.flags;
   const flagPicture = Object.values(flags)[0];
-  const flagAlt = Object.values(flags)[2];
+  const flagAlt = Object?.values(flags)[2];
 
   return (
     <div className='max-w-fit mx-auto'>
@@ -14,7 +14,7 @@ const Country = ({ country }) => {
       </div>
       <div className='rounded shadow-lg hover:cursor-pointer'>
         <div className=''>
-          <img className='w-[256px] h-[180px] object-fill rounded-t-lg flag' src={flagPicture} alt={flagAlt} />
+          <img className='w-[256px] h-[180px] object-fill rounded-t-lg flag' src={flagPicture} alt={flagAlt && flagAlt.length <= 0 ? `Flag of ${name.common}` : flagAlt} />
         </div>
         <div className='text-left px-6 pt-6 pb-10 dark:text-white dark:bg-darkElement rounded-b-lg'>
           <h1 className='font-bold text-lg pb-4 pt-3'>{name.common.length > 19 ? `${name.common.substring(0, 19)}...` : name.common}</h1>
